fc2dc755fe37c63292a8c5cb0eb0191ea1fd4150
qiang.li <jonelee05@qq.com>
2018-03-30 14:15:02 +0800

    Async thread pool,ThreadPoolTaskExecutor

--- src/main/java/trapReceiverApplication.java
+++ src/main/java/trapReceiverApplication.java
@@ -10,13 +10,29 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = "cn.bg.trap.business")
 public class trapReceiverApplication {
-    @Autowired
-    private void setSnmpTrapReceiver(SNMPTrapReceiver snmpTrapReceiver){
-        trapReceiverApplication.snmpTrapReceiver = snmpTrapReceiver;
-    }
     static SNMPTrapReceiver snmpTrapReceiver;
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(trapReceiverApplication.class, args);
         snmpTrapReceiver.run();
     }
+
+    @Autowired
+    private void setSnmpTrapReceiver(SNMPTrapReceiver snmpTrapReceiver) {
+        trapReceiverApplication.snmpTrapReceiver = snmpTrapReceiver;
+    }
+
+//    @Bean(destroyMethod = "shutdown",name="trapexcutor")
+//    public Executor getTrapExecutor() {
+//        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+//        executor.setCorePoolSize(5);
+//        executor.setMaxPoolSize(10);
+//        executor.setQueueCapacity(100);
+//        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+//        executor.setAllowCoreThreadTimeOut(true);
+//        executor.initialize();
+//        return executor;
+//    }
+
+    //@Async("trapexcutor")
 }
